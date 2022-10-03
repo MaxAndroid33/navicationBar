51411a766111da06daa34686cf887491aa369bf7
MaxAndroid33 <yuosifmax309@gmail.com>
2022-10-04 02:21:19 +0300

    design the dropdown bar for only the desktop screen

--- js/app.js
+++ js/app.js
@@ -6,12 +6,14 @@ const menu =document.querySelector('.menu');
 const logo =document.querySelector('.brand');
 const old_logo =document.createElement('img');
 const new_logo =document.createElement('img');
+const dropdownactive = document.querySelectorAll('.nav-item');
+
 old_logo.setAttribute('src','../image/union red1_new2.svg');
 old_logo.classList.add('logo');
 new_logo.setAttribute('src','../image/union red2.svg');
 new_logo.classList.add('logo');
 // PX offset when the navbar active
-const offset =300;
+const offset =200;
 
 // menu event click
 menBtn.addEventListener('click' ,function(){
@@ -24,10 +26,18 @@ window.addEventListener('scroll',function(){
 
     if (this.pageYOffset > offset ){
         navbar.classList.add('navbar-active');
+       dropdownactive.forEach(item =>{
+        item.classList.add('nav-item-active');
+       });
        logo.replaceChild(new_logo,logo.lastChild);
+        
     }
     else{
         navbar.classList.remove('navbar-active');
+        dropdownactive.forEach(item =>{
+            item.classList.remove('nav-item-active');
+           });
         logo.replaceChild(old_logo,logo.lastChild);
+       
     }
 });
